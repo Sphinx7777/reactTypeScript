ec02ce7616ba9971a5c7e649b3c5dc6dbfafb9f8
Sphinx7777 <spamoglot13@gmail.com>
2019-10-19 03:49:02 +0300

    Updates

---
+++ precache-manifest.687e5c234222126900d882088eddafb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5489f89537e1159ea779f66bc4b5d7ad",
+    "url": "/reactTypeScript/index.html"
+  },
+  {
+    "revision": "1204cd3c3d42b412d54b",
+    "url": "/reactTypeScript/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "f87cab23dde1736447a9",
+    "url": "/reactTypeScript/static/css/main.2a28a42b.chunk.css"
+  },
+  {
+    "revision": "1204cd3c3d42b412d54b",
+    "url": "/reactTypeScript/static/js/2.05165874.chunk.js"
+  },
+  {
+    "revision": "f87cab23dde1736447a9",
+    "url": "/reactTypeScript/static/js/main.09e15679.chunk.js"
+  },
+  {
+    "revision": "f4dc5fe8bb740abeb629",
+    "url": "/reactTypeScript/static/js/runtime-main.be720d64.js"
+  }
+]);
