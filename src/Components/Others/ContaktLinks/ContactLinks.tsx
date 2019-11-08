b82f8eac457759a646eb9220efae0e4a07b0827c
Sphinx7777 <spamoglot13@gmail.com>
2019-11-08 22:52:23 +0200

    todoHeader

--- src/Components/Others/ContaktLinks/ContactLinks.tsx
+++ src/Components/Others/ContaktLinks/ContactLinks.tsx
@@ -5,14 +5,21 @@ const lamp = require('./../../../images/lamp.ico');
 const telegram = require('./../../../images/telegram.ico');
 const linkedIn = require('./../../../images/linkedIn.ico');
 const git = require('./../../../images/git.ico');
+const network = require('./../../../images/socialnetwork.png');
 
 
 export const ContactLinks = () => {
 	return (
 		<div className={s.contactsLinkWrapper}>
 			<a className={s.link} target='_blank' rel='noopener noreferrer'
+				 href="https://sphinx7777.github.io/SocialNetvork">
+				<img className={s.linkImg} src={network} alt="Social network"/>
+				<span className={s.linkTitle}>Social Network</span>
+			</a>
+			<a className={s.link} target='_blank' rel='noopener noreferrer'
 				 href="https://djinni.co/q/03c688fb54">
-				<img className={s.linkImg} src={lamp} alt="Джинни"/><span className={s.linkTitle}>Djinni</span>
+				<img className={s.linkImg} src={lamp} alt="Джинни"/>
+				<span className={s.linkTitle}>Djinni</span>
 			</a>
 			<a className={s.link} target='_blank' rel='noopener noreferrer'
 				 href="https://www.linkedin.com/in/onishchenkosergei">
